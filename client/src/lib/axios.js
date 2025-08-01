98a2293c13d9e93ef0305e5d39dcc10e606aca85
mr-rishikesh <mr.rishikesh2@gmail.com>
2025-08-01 14:55:44 +0530

    backend url changed for the cookie issue

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 // this is for the connect to backend api 
 export const axiosInstance = axios.create({
-    baseURL : import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://devconnect-1-6ivl.onrender.com",
+    baseURL : import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://devconnect-1-26zi.onrender.com",
     withCredentials : true 
 })
