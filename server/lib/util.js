0f97fa4f9ec38052230897254b844c69841a181e
mr-rishikesh <mr.rishikesh2@gmail.com>
2025-08-01 14:31:33 +0530

    fixing cookies issue

--- server/lib/util.js
+++ server/lib/util.js
@@ -12,7 +12,7 @@ export const generateToken = async (userId , res) => {
         res.cookie("jwt" , token , {
         maxAge : 7*24*60*60*1000 , //ms
         httpOnly : true ,
-        sameSite : "strict" ,
+        sameSite : "none" ,
         secure : process.env.NODE_ENV != "development"
     })
 
