fbfe41250320b2aefa2477d586e90dc6d23b2835
deepanshu-prajapati01 <deepanshubawaliya@gmail.com>
2025-08-12 12:10:58 +0530

    Updated post and user models
    Post model changes:
      - Refactored code, removing unnecessary spaces and beautifying code
      - Added comments for better understanding
      - Using description as short description and content as the main content of the post
      - Using upvotedBy as an array of objects containing userId and upvotes as the total number of upvotes
    
    User model changes:
      - Refactored code, removing unnecessary spaces and beautifying code
      - Added a default profile pic in user model

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -1,70 +1,58 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
+
 
 const experienceSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  startDate: { type: Date, required: true },
-  
-  companey : {
-    type : String ,
-    
-  },
-  endDate: Date
-}); 
+    title: { type: String, required: true },
+    description: String,
+    startDate: { type: Date, required: true },
 
-const userSchema = new  mongoose.Schema({
-    email : {
-        type : String ,
-        required : true ,
-        unique : true 
-
-    } ,
-
-
-    fullName : {
-        type : String ,
-        required : true ,
+    companey: {
+        type: String,
 
     },
-    title : {
-        type : String ,
-        default : "Developer"
-        
+    endDate: Date
+});
 
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-
-    password : {
-        type : String ,
-        required : true ,
-        minlength : 6
+    fullName: {
+        type: String,
+        required: true,
     },
-    profilePic : {
-        type : String ,
+    title: {
+        type: String,
+        default: "Developer"
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    profilePic: {
+        type: String,
+        default: "https://i.pinimg.com/1200x/97/17/1b/97171bd3726ad80a25d7ce7d5028db87.jpg"
+    },
+    portfolio: {
+        type: String,
         default: ""
-    } ,
-    portfolio : {
-        type : String ,
+    },
+    resume: {
+        type: String,
         default: ""
-    } ,
-    resume : {
-        type : String ,
-        default: ""
-    } ,
-
-
-    techStack : {
-       type :  [String] ,
-       default : ["full stack", "developer","frontend"]
-    } , 
+    },
+    techStack: {
+        type: [String],
+        default: ["full stack", "developer", "frontend"]
+    },
     experience: { type: [experienceSchema], default: [] }
 
 
+}, { timestamps: true })
 
-
-   
-
-} , {timestamps : true})
-
-const User = mongoose.model("User" , userSchema)
+const User = mongoose.model("User", userSchema)
 
 export default User
