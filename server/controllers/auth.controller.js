aa17198361aa1e87a8141086564ad5cb1d7af46e
deepanshu-prajapati01 <deepanshubawaliya@gmail.com>
2025-08-13 14:14:19 +0530

    Beautify auth controller and fixed some issues
    - Removed password and other fields from response.
    - Apply proper code formatting

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -8,10 +8,7 @@ import { generateToken } from "../lib/util.js";
 import bcrypt from "bcrypt";
 
 export const signUp = async (req, res) => {
-  const {email, fullName, githubUrl,  password } = req.body;
-  console.log(email)
-
- 
+  const { email, fullName, githubUrl, password } = req.body;
 
   try {
     const isUserExist = await User.findOne({ email });
@@ -32,23 +29,37 @@ export const signUp = async (req, res) => {
       email,
 
       password: hashedPassword,
-   
+
     });
     await newUser.save();
 
-        if (newUser) {
-       const token = await generateToken(newUser._id, res);
-       console.log("This is token" + token)
-      
-      return res.status(201).json({
-        message: "Successfully Otp sent",
-        data: newUser,
-        token,
-      });
+    // todo: Needs to implement logic of sending otp
+
+    if (newUser) {
+      console.log(newUser)
+      const token = generateToken(newUser._id, res);
+      console.log("This is token" + token)
+
+
+      // remove unnecessary data from response
+      newUser.password = undefined;
+      newUser._id = undefined;
+      newUser.__v = undefined;
+      newUser.createdAt = undefined;
+      newUser.updatedAt = undefined;
+
+      return res
+        .status(201)
+        .json({
+          message: "Successfully Otp sent",
+          data: newUser,
+          token,
+        });
 
     }
 
   } catch (error) {
+    console.log(error)
     res
       .status(500)
       .json({ success: false, message: "Failed to send OTP.", error: error });
@@ -57,59 +68,59 @@ export const signUp = async (req, res) => {
 
 
 
-export const signin = async (req , res) => {
-    const {email , password} = req.body;
-    if(!email || !password) {
-        return res.status(401).json({
-                message : "Put all credentals  "
-            })
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(401).json({
+      message: "Put all credentals  "
+    })
+  }
+
+  try {
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials  ,User not exists "
+      })
+
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(403).json({
+        message: "Invalid credentials"
+      })
     }
 
-    try {
+    const token = await generateToken(user._id, res)
+    console.log("This is token" + token)
 
-        const user = await User.findOne({email});
-        if(!user) {
-            return res.status(401).json({
-                message : "Invalid credentials  ,User not exists "
-            })
+    return res.status(200).json({
+      message: "Successfully logined ",
+      data: user,
+      token
+    })
 
-        }
-        const isPasswordMatch = await  bcrypt.compare(password , user.password);
-        if(!isPasswordMatch) {
-            return res.status(403).json({
-                message : "Invalid credentials"
-            })
-        }
+  } catch (error) {
 
-        const token =await  generateToken(user._id , res)
-        console.log("This is token" + token)
+    res.status(500).json({ success: false, message: "Something wrong in signup" })
 
-        return res.status(200).json({
-            message : "Successfully logined " ,
-            data : user ,
-            token
-        })
-        
-    } catch (error) {
-
-        res.status(500).json({success : false , message : "Something wrong in signup"})
-        
-    }
+  }
 }
 
-export const checkAuth = async  (req , res) => {
-    try {
+export const checkAuth = async (req, res) => {
+  try {
 
-      
-     const allUsers = await User.find();
-        
-        res.status(200).json({authUser :req.user , allUsers : allUsers});
-        
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            message : "Something wrong in checkAuth"
-        })
-        
-    }
+
+    const allUsers = await User.find();
+
+    res.status(200).json({ authUser: req.user, allUsers: allUsers });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Something wrong in checkAuth"
+    })
+
+  }
 }
